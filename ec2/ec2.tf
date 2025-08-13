552b25871027521b6943a379fd0313e2dfbc452c
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-08-13 11:37:57 +0530

    k8

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,37 +1,41 @@
-# resource <resource-type> <resource-name>
+resource "aws_key_pair" "ssh_key" {
+  key_name   = "ssh-agent-key"
+  public_key = file("C:/Users/hp/Desktop/ec2pem/ec2-Raj.pem") # Make sure this is the PUBLIC key
+}
 resource "aws_instance" "db" {
-    ami ="ami-09c813fb71547fc4f"
-    vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-    instance_type = "t3.micro"
+  ami                    = "ami-09c813fb71547fc4f"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  key_name               = aws_key_pair.ssh_key.key_name
 
-    tags = {
-        Name = "work-station"
-    }
+  tags = {
+    Name = "work-station"
+  }
 }
 
 resource "aws_security_group" "allow_ssh" {
-    name = "allow_ssh"
-    description = "allowing SSH access"
+  name        = "allow_ssh"
+  description = "allowing SSH access"
 
-    #terraform block
-    ingress {
-        from_port        = 22
-        to_port          = 22
-        protocol         = "tcp"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port        = 0 # from 0 to 0 means, opening all protocols
-        to_port          = 0
-        protocol         = "-1" # -1 all protocols
-        cidr_blocks      = ["0.0.0.0/0"]
-    }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        Name = "allow_ssh"
-        CreatedBy = "Hemanth"
-    }
+  tags = {
+    Name       = "allow_ssh"
+    CreatedBy  = "Hemanth"
+  }
 }
+
 
 
